9c2f4933fe28c2a2f80bdaf921db60188d20540a
Albert Sanchez <albert.san.iza@gmail.com>
2016-05-20 22:40:34 -0700

    added tabs view

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -28,4 +28,23 @@ angular.module('starter.controllers', [])
       alert('Unable to get location: ' + error.message);
     });
   };
+})
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
 });

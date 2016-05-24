d384983db77f4093985952e154d79f8104ecef05
Albert Sanchez <albert.san.iza@gmail.com>
2016-05-24 16:48:02 -0700

    Creating the proximiio API

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -13,32 +13,57 @@ angular.module('starter.controllers', [])
 //------------------------------------------------------------------------------
 //------------------------------------------------------------------------------
 .controller('ProximiioCtrl', function($scope, myProximiio) {
-
   $scope.text_test = "Este texto es de prueba";
-
   $scope.Login = function() {
     myProximiio.Login({email: 'albert.san.iza@gmail.com', password: 'rsy52m7a'}).success(function(data) {
-      console.log(data);
       $scope.text_test = data;
     }).error(function(e) {
-      console.log(e);
+      $scope.text_test = e;
+    });
+  };
+  $scope.Registration = function() {
+    myProximiio.Registration();
+  };
+  $scope.Analytics_Overview = function() {
+    myProximiio.Analytics_Overview({date_from: '2016-01-01', date_to: '2016-04-01', place_id: ''}).success(function(data) {
+      $scope.text_test = data;
+    }).error(function(e) {
+      $scope.text_test = e;
+    });
+  };
+  $scope.Analytics_Visitor = function() {
+    myProximiio.Analytics_Visitor({id: '538a33b7-195d-4431-8de0-4bf50154afac'}).success(function(data) {
+      $scope.text_test = data;
+    }).error(function(e) {
+      $scope.text_test = e;
+    });
+  };
+  $scope.Geofences_All = function() {
+    myProximiio.Geofences_All({limit: '10', skip: '0'}).success(function(data) {
+      $scope.text_test = data;
+    }).error(function(e) {
+      $scope.text_test = e;
+    });
+  };
+  $scope.Geofences_Specific = function() {
+    myProximiio.Geofences_Specific({id: '91c88926-86b0-45e6-bc6d-1e3c9ee491c2'}).success(function(data) {
+      $scope.text_test = data;
+    }).error(function(e) {
+      $scope.text_test = e;
+    });
+  };
+  $scope.Geofences_Create = function() {
+    myProximiio.Geofences_Create({name: 'Test Geofence', address: 'Shaftesbury Avenue 4', area: {lat: '53.39165', lng: '-2.32738'}, radius: '8'}).success(function(data) {
+      $scope.text_test = data;
+    }).error(function(e) {
       $scope.text_test = e;
     });
   };
 
-
-
-  $scope.test = function() {
-    $http.get('https://api.proximi.fi/core/geofences?limit=10&skip=0', {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + myProximiio.token
-      }
-    }).success(function(data) {
-      console.log(data[0]);
+  $scope.Geofences_Update = function() {
+    myProximiio.Geofences_Update({id: 'bfebdeb4-0311-4680-a5ad-c30406ae154d'}, {name: 'Super Test Geofence'}).success(function(data) {
       $scope.text_test = data;
     }).error(function(e) {
-      console.log("Error: " + e);
       $scope.text_test = e;
     });
   };
--- www/js/services.js
+++ www/js/services.js
@@ -14,12 +14,78 @@ angular.module('starter.services', [])
   // Login
   // This endpoint returns an authorization token.
   this.Login= function(param) {
+    // email
+    // password
     return $http.post(urlBase + 'core_auth/login', param);
   };
   //----------------------------------------------------------------------------
   // Registration
   // This endpoint returns an authorization token, user object and organization object.
   this.Registration = function(param) {
+    // email
+    // password
+    // first_name
+    // last_name
+    // background
+    // country
+    console.log("Registration: Nothing to do here...");
+  };
+  //----------------------------------------------------------------------------
+  // Analytics
+  // This endpoint returns analytic overview.
+  this.Analytics_Overview = function(param) {
+    // date_from
+    // date_to
+    //// place_id
+    return $http.get(urlBase + 'core/analytics/overview?date_from=' + param.date_from + '&date_to=' + param.date_to, {headers});
+  };
+  // This endpoint returns specific visitor analytic overview.
+  this.Analytics_Visitor = function(param) {
+    // id
+    return $http.get(urlBase + 'core/analytics/visitor/:id?id' + param.id, {headers});
+  };
+  //----------------------------------------------------------------------------
+  // Geofences
+  // This endpoint retrieves all geofences. GeoSearch can be enabled by adding all optional search_* query parameters.
+  this.Geofences_All = function(param) {
+    // limit
+    // skip
+    //// search_latitude
+    //// search_longitude
+    //// search_radius
+    //// search_distance_order
+    return $http.get(urlBase + 'core/geofences?limit=' + param.limit + '&skip=' + param.skip, {headers});
+  };
+  // This endpoint retrieves a specific geofence.
+  this.Geofences_Specific = function(param) {
+    // id
+    return $http.get(urlBase + 'core/geofences/' + param.id, {headers});
+  };
+  // This endpoint creates a specific geofence.
+  this.Geofences_Create = function(param) {
+    // name
+    // address
+    // area.lat
+    // area.lng
+    // radius
+    return $http.post(urlBase + 'core/geofences', param, {headers});
+  };
+  //----------------------------------------------------------------------------
+  // This endpoint updates a specific geofence.
+  this.Geofences_Update = function(param_put, param) {
+    // id
+    // name
+    // address
+    // area.lat
+    // area.lng
+    // radius
+    return $http.put(urlBase + 'core/geofences/' + param_put.id, param, {headers});
+  };
+  //----------------------------------------------------------------------------
+  // This endpoint deletes a specific geofence.
+  this.Geofences_Delete = function(param) {
+    // id
+    return $http.delete(urlBase + 'core/geofences/', {headers});
   };
   //----------------------------------------------------------------------------
 })

0bada80a5a3b1d119a5a71e80482be320b0258ed
Albert Sanchez <albert.san.iza@gmail.com>
2016-07-19 17:38:37 -0700

    bootstraped translate on register

--- www/js/translate.js
+++ www/js/translate.js
@@ -10,7 +10,18 @@ angular.module('starter.translate', ['pascalprecht.translate'])
       '_ENTER_PASSWORD': 'Enter your team password',
       '_SIGN_IN_NOW': 'SIGN IN NOW',
       '_PBS': 'Provided by uCanSee Staff',
-
+      '_REGISTER_INFO_1': 'If you are interested in participating, please send an e-mail with the following information:',
+      '_REGISTER_INFO_2': 'Team Name',
+      '_REGISTER_INFO_3': 'Members list with their "Student Number"',
+      '_REGISTER_INFO_4': 'The e-mail should be sent to albert.sanchez@cetys.mx',
+      '_RULES': 'Rules',
+      '_RULES_INFO_1': 'Teams must be composed of six members. All participants must be students of CETYS University and must follow the organizers instructions. Physical or verbal violence will result in immediate disqualification of all the members of the team',
+      '_SPONSORS': 'Sponsors',
+      '_SPONSORS_INFO_1': 'To be defined...',
+      '_HELP': 'Help',
+      '_HELP_INFO_1': 'For any questions please contact albert.sanchez@cetys.mx',
+      '_CREDITS': 'Provided by uCanSee Staff',
+      '_CREDITS_INFO_1': 'This app was made by students of CETYS University using amazing technologies like Ionic, Proximi.io, Firebase and Cordova',
     });
     $translateProvider.translations('es', {
       '_REGISTER': 'REGISTRO',

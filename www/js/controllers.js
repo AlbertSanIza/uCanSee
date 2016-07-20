25e13405f77be5eeef583940007b26737fae0e78
Albert Sanchez <albert.san.iza@gmail.com>
2016-07-19 18:08:48 -0700

    deleted extra code

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -122,20 +122,6 @@ angular.module('starter.controllers', [])
     timeout: 10000,
     enableHighAccuracy: false
   };
-  /*
-  var watchOptions = {
-    timeout : 3000,
-    enableHighAccuracy: false
-  };
-  var watch = $cordovaGeolocation.watchPosition(watchOptions);
-  watch.then(null, function(err) {
-    // error
-  }, function(position) {
-    var lat  = position.coords.latitude
-    var long = position.coords.longitude
-    $scope.myPosition = [position.coords.latitude, position.coords.longitude];
-  });
-  */
   $scope.findMe = function() {
     $ionicLoading.show({
       template: '<ion-spinner icon="spiral" class="spinner-energized"></ion-spinner><br>{{text}}...',

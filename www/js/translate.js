7290ee7f31ea1e8387fad0b79358a807dcb92a10
Albert Sanchez <albert.san.iza@gmail.com>
2016-07-19 17:27:07 -0700

    started register translate

--- www/js/translate.js
+++ www/js/translate.js
@@ -4,20 +4,23 @@ angular.module('starter.translate', ['pascalprecht.translate'])
 .config(function($translateProvider) {
   $translateProvider.translations('en', {
       '_REGISTER': 'REGISTER',
+      '_REGISTER_2': 'Register',
       '_SIGN_IN': 'SIGN IN',
       '_SELECT_TEAM': 'Select your team',
       '_ENTER_PASSWORD': 'Enter your team password',
       '_SIGN_IN_NOW': 'SIGN IN NOW',
       '_PBS': 'Provided by uCanSee Staff',
+
     });
     $translateProvider.translations('es', {
       '_REGISTER': 'REGISTRO',
+      '_REGISTER_2': 'Registro',
       '_SIGN_IN': 'INICIAR SESION',
       '_SELECT_TEAM': 'Selecciona a tu equipo',
       '_ENTER_PASSWORD': 'Ingresa la contraseña de tu equipo',
       '_SIGN_IN_NOW': 'INICIAR SESION YA',
       '_PBS': 'Proveida por uCanSee Staff',
     });
-    $translateProvider.preferredLanguage('en');
+    $translateProvider.preferredLanguage('es');
 })
 //------------------------------------------------------------------------------
